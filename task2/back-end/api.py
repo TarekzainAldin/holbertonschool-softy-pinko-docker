db1bb02b86ac706d707895b4be8f0b2fd26d8407
TAREKZAINALDIN <tarekzindin@gmail.com>
2024-08-28 15:19:14 +0200

    MAKE CLON FOR THE GIT HUB INDEX FILE

--- task2/back-end/api.py
+++ task2/back-end/api.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template ,url_for ,redection ,render_template
+from flask import Flask
 
 app = Flask(__name__)
 
@@ -8,3 +8,4 @@ def hello_world():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5252)
+    
